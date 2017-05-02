94e9efe281a9e7fbac60c7f6a74bb84cee08a2c0
Yasir Hossain <yhossain@gmail.com>
2017-05-02 15:03:14 -0700

    updated contact styling

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 
 export default () => {
   return (
-    <div>
+    <div className='contact component-container box-shadow'>
       <h1>Contact</h1>
-      <p>
-        <div>
-          <label>Github</label><label>yasirhossain</label>
+      <div>
+        <div className='card'>
+          <label>Github</label>
+          <a href='https://github.com/yasirhossain' target='_blank'>yasirhossain</a>
         </div>
-        <div>
+        <div className='card'>
           <label>Twitter</label>
-          <label>@ctrlaltdeceit</label>
+          <a href='https://twitter.com/ctrlaltdeceit' target='_blank'>@ctrlaltdeceit</a>
         </div>
-        <div>
+        <div className='card'>
           <label>LinkedIn</label>
-          <label>yasirhossain</label>
+          <a href='https://linkedin.com/in/yasirhossain' target='_blank'>yasirhossain</a>
         </div>
-        <div>
+        <div className='card'>
           <label>Instagram</label>
-          <label>@ctrlaltdeceit</label>
+          <a href='https://www.instagram.com/ctrlaltdeceit/' target='_blank'>@ctrlaltdeceit</a>
         </div>
-      </p>
+      </div>
     </div>
   );
 };
