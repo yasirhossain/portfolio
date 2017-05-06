8c22c752f77d9e9d6ac61972c5b71f829effdbf0
Yasir Hossain <yhossain@gmail.com>
2017-05-05 21:36:09 -0700

    added image loader to webpack

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -5,6 +5,13 @@ import firebase from 'firebase';
 
 import Hearts from './hearts/hearts_index';
 
+import heroImage from '../static/sample.png';
+
+const heroImageStyle = {
+  background: `url(${heroImage}) 10% center no-repeat transparent`,
+  backgroundSize: `contain`
+}
+
 export default () => {
   return (
     <div className="about section">
@@ -13,14 +20,14 @@ export default () => {
         <h2><span className="call-out">Hello</span> my name is</h2>
         <h1>Yasir Hossain</h1>
         <p>
-          I posses a unique blend of UX / UI Design, Full-Stack Engineering. Passion for creating highly performant, scalable and interactive components from ideation, design, development, all the way to deployment.
+          I possess a unique blend of UX / UI Design, Full-Stack Engineering. Passion for creating highly performant, scalable and interactive components from ideation, design, development, all the way to deployment.
         </p>
         <p>
           <span className="badge">TLDR</span> A Creative Technologist with a passion to solve problems with a focus on innovative hollistic approach.
         </p>
       </div>
       <div className="hero-container">
-        <div className="hero"></div>
+        <div className="hero" style={heroImageStyle}></div>
       </div>
     </div>
   );
